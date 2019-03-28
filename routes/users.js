31f7f0f2a464c8ff6b735b1c254229fd0973f2cb
ethan <ethanwang90@gmail.com>
2019-03-28 22:05:00 +1300

    prevent CORS

---
+++ routes/users.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.get('/test', function(req, res, next) {
+  res.status(200).json({
+    message: 'json response test'
+  });
+})
+
+module.exports = router;
