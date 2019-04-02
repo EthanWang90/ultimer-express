2e860f33e46b7a3c7e08dcbeb692d08c29cdff66
ethan <ethanwang90@gmail.com>
2019-04-02 22:24:04 +1300

    add socket.io

--- routes/users.js
+++ routes/users.js
@@ -3,11 +3,19 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('respond with a resource again');
 });
 
 router.get('/test', function(req, res, next) {
   res.status(200).json({
+    resArr : [
+      {
+        name : 'jack'
+      },
+      {
+        name : 'rose'
+      }
+    ],
     message: 'json response test'
   });
 })
